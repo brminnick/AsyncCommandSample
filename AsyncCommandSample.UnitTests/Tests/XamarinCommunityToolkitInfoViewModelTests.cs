3477eda9ad55029c8e3f6c75ea09a4b964b9cc1d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-02-09 11:04:57 -0800

    Initial commit

---
+++ AsyncCommandSample.UnitTests/Tests/XamarinCommunityToolkitInfoViewModelTests.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AsyncCommandSample.UnitTests
+{
+    class XamarinCommunityToolkitInfoViewModelTests
+    {
+        [Test]
+        public async Task GetLatestReleaseTest()
+        {
+            // Arrange
+            string? latestRelease_Initial, latestRelease_Final;
+
+            var xamarinCommunityToolkitInfoViewModel = new XamarinCommunityToolkitInfoViewModel(new MockPreferences());
+
+            // Act
+            latestRelease_Initial = xamarinCommunityToolkitInfoViewModel.LatestRelease;
+
+            await xamarinCommunityToolkitInfoViewModel.GetLatestRelease.ExecuteAsync();
+
+            latestRelease_Final = xamarinCommunityToolkitInfoViewModel.LatestRelease;
+
+            // Assert
+            Assert.IsNull(latestRelease_Initial);
+            Assert.IsNotNull(latestRelease_Final);
+        }
+    }
+}
