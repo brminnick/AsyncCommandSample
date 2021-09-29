f6f8919427509d1d5f9bd71c0190ed88454689e5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-09-28 17:57:51 -0700

    Update NuGet Packages

--- AsyncCommandSample.iOS/Main.cs
+++ AsyncCommandSample.iOS/Main.cs
@@ -4,6 +4,6 @@ namespace AsyncCommandSample.iOS
 {
     public class Application
     {
-        static void Main(string[] args) => UIApplication.Main(args, null, nameof(AppDelegate));
+        static void Main(string[] args) => UIApplication.Main(args, null, typeof(AppDelegate));
     }
 }
